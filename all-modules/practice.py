7d98a34d31ee55952f3476a228498070f34f4a03
JakeRobitaille <jar1994.learn@gmail.com>
2023-02-19 09:50:30 -0500

    Add three dart out mode and init until out mode

--- all-modules/practice.py
+++ all-modules/practice.py
@@ -91,40 +91,73 @@ class DUB_TRIP:
 # Set up class to output a random number between 2-170 with input to show user how many points they have left
 class rand_out:
     def __init__(self):
-        self.roll = None
+        # Sets up lists with all out possibilities with 3 or less darts 
+        self.this_out = None
+        self.this_hit = None
         self.lowEven = [i for i in range(2, 41) if i % 2 == 0]
         self.lowOdd = [i for i in range(3, 42) if i % 2 == 1]
         self.midEven = [i for i in range(42, 61) if i % 2 == 0]
         self.midOdd = [i for i in range(43, 60) if i % 2 == 1]
         self.highOut = [i for i in range(61, 99)]
         self.higherOut = [i for i in range(99, 119)]
-        self.crazyOut = [i for i in range(119, 171) if i != 159 or i != 162 or i != 163 or i != 165 or i != 166 or i != 168 or i != 169] 
+        self.crazyOut = [i for i in range(119, 161) if i != 159]
+        for i in range(161, 171, 3):
+            self.crazyOut.append(i) 
     
-    # Sets up which number sets to chose between
-    def Prob_Roll(self):
-        return random.randint(0, 34)
+    def Num_Output(self, outList):
+        self.this_out = random.choice(outList)
+        print(f'Your random out is: {self.this_out}')
     
-    def your_out(self):
-        roll = self.Prob_Roll()
-        print(roll)
+    # Player will need to out in three darts or less
+    def three_out(self):
+        # Sets up which number sets to choose between
+        roll = random.randint(0, 34)
+        if roll == 0:
+            self.Num_Output(self.crazyOut)
+        elif roll > 0 and roll <= 3:
+            self.Num_Output(self.higherOut)
+        elif roll > 3 and roll <= 8:
+            self.Num_Output(self.lowEven)
+        elif roll > 8 and roll <= 12:
+            self.Num_Output(self.highOut)
+        elif roll > 12 and roll <= 19:
+            self.Num_Output(self.midOdd)
+        elif roll > 19 and roll <= 27:
+            self.Num_Output(self.midEven)
+        else:
+            self.Num_Output(self.lowOdd)
         
-
+        # Sets up tracker for points left after something is hit
+        outTracker = self.this_out
+        
+        # Three darts to get something down
+        for i in range(3):
+            print(f'Turn {i + 1}:')
+            self.this_hit = int(input('How many points did you hit?   -----> '))
+            outTracker -= self.this_hit
+            
+            if outTracker == 0:
+                print('Congrats, you got your out in 3 darts or less!')
+                break
+            else:
+                print(f'You have {outTracker} points left \n')
+                
+        if outTracker > 0:
+            print('You were unable to out in 3 darts')
+            
+    def until_out(self):
+        pass
+            
+        
 def main():
     pass 
 
 # dd = DUB_TRIP()
-# game_1 = dd.dart_track('triple')
+# game_1 = dd.dart_track('double')
 # print(game_1)
 
-db = BULLS()
-game_2 = db.practice_bulls()
+# db = BULLS()
+# game_2 = db.practice_bulls()
 
-# ro = rand_out()
-# print(f'''{ro.lowEven}
-#       {ro.lowOdd}
-#       {ro.midEven}
-#       {ro.midOdd}
-#       {ro.highOut}
-#       {ro.higherOut}
-#       {ro.crazyOut}
-#       ''')
+ro = rand_out()
+ro.three_out()
